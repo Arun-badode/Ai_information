9f1aa9981aa5b5e8024d1b714b7258e559503fb1
pradeep meena <pradeepwork.9516@gmail.com>
2025-05-20 19:14:58 +0530

    changes

--- src/AI_tools/Details/AI Chatbots & Assistants/DriftAI.jsx
+++ src/AI_tools/Details/AI Chatbots & Assistants/DriftAI.jsx
@@ -605,7 +605,7 @@ const PiAI = () => {
       </div>
       <div className="bg-dark text-light min-vh-100 py-5">
         <div className="container">
-          <div className="d-flex align-items-center justify-content-between mb-2">
+          <div className="d-flex align-items-center justify-content-between mb-2 lovaaiflex">
             {/* Left side (Text Section) */}
             <div style={{ flex: 1, paddingRight: "30px" }}>
               <h2>PI AI</h2>
