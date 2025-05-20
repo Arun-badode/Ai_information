31eaea352f815a8d54b48ea376c318d09ce4f020
pradeep meena <pradeepwork.9516@gmail.com>
2025-05-20 18:55:23 +0530

    changes

--- src/AI_tools/Details/AI Chatbots & Assistants/ReplikaAI.jsx
+++ src/AI_tools/Details/AI Chatbots & Assistants/ReplikaAI.jsx
@@ -661,7 +661,7 @@ const ReplikaAI = () => {
       </div>
       <div className="bg-dark text-light min-vh-100 py-5">
         <div className="container">
-          <div className="d-flex align-items-center justify-content-between mb-5">
+          <div className="d-flex align-items-center justify-content-between mb-5 lovaaiflex">
             {/* Left side (Text Section) */}
             <div style={{ flex: 1, paddingRight: "30px" }}>
               <img
